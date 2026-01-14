6540fbb56a80b0b76ab88cd72890eb58bbe898c1
Alex <10946053+alexwoodle@users.noreply.github.com>
2026-01-13 20:33:57 -0500

    add nav and config

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.write("Hello world")
+# st.markdown(
+#     """
+#     <style>
+#     [data-testid="stToolbar"] {
+#         visibility: hidden;
+#         height: 0;
+#         position: fixed;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True,
+# )
+
+
+def home():
+    st.title("Home")
+
+
+pg = st.navigation([home, "pages/page_1.py", "pages/page_2.py"])
+pg.run()
